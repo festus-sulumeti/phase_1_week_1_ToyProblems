2e9069c146c04d88ed35eeae4657230cc6f0188d
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-25 11:18:31 +0300

    Completed assignment

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -1,3 +1,15 @@
+/*
+This program calculates the net salary of an employee, taking into account a 20% income tax deduction.
+
+The program first imports the readline module, which allows it to read user input from the console. It then creates a new readline interface object, which will be used to communicate with the user.
+
+Next, the program defines a function called calculateNetSalary(). This function takes a single argument, the employee's salary in number. It then calculates the net salary by subtracting the income tax deduction from the employee's salary.
+
+The calculateNetSalary() function then prints a message to the console saying what the employee's net salary is.
+
+Finally, the program prompts the user for the employee's salary. It then converts the user's input to a number and calls the calculateNetSalary() function to calculate the employee's net salary. The program then prints the employee's net salary to the console and closes the readline interface object.
+*/
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
